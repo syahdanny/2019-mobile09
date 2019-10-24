584d7dee2da8e7758c13a6e40241bb0fd067954a
syahdanny <syahdanny11@gmail.com>
2019-10-24 13:45:27 +0700

    Recycle view sederhana selesai

--- RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/MainActivity.java
+++ RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/MainActivity.java
@@ -1,14 +1,40 @@
 package id.ac.polinema.recyclerviewsangatsederhana;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import id.ac.polinema.recyclerviewsangatsederhana.adapters.SuperHeroAdapter;
+import id.ac.polinema.recyclerviewsangatsederhana.models.SuperHero;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvSuperHero;
+
+    List<SuperHero> listSuperHero = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvSuperHero = findViewById(R.id.rvSuperHero);
+        SuperHero hero = new SuperHero("Petruk");
+        //menambahkan hero ke listSuperHero
+        listSuperHero.add(hero);
+        //membuat object hero baru
+        hero = new SuperHero("Gareng");
+        //menambahkan hero baru ke listSuperhero
+        listSuperHero.add(hero);
+
+        SuperHeroAdapter superHeroAdapter = new SuperHeroAdapter(listSuperHero);
+
+        rvSuperHero.setAdapter(superHeroAdapter);
+        rvSuperHero.setLayoutManager(new LinearLayoutManager(this));
     }
 }
